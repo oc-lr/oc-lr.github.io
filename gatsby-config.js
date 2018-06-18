a97d254a056c3a43339bfa5d5000c5e974348ab3
Jikku Jose <jikkujose@gmail.com>
2018-06-18 06:26:30 +0530

    Update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "OrigoChain: Land Records",
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ["gatsby-plugin-react-helmet"],
 }
