2a8eb4125927e833ef205688b53fd1cd0cfc4049
Jikku Jose <jikkujose@gmail.com>
2018-06-18 06:08:10 +0530

    Remove White Paper link

--- src/components/StartPage/Footer.js
+++ src/components/StartPage/Footer.js
@@ -2,7 +2,6 @@ import React from "react"
 
 export const Footer = () => (
   <div className="flex open-sans fw4">
-    <div className="pointer mr3 hover-gray b">White Paper</div>
     <div className="pointer mr3 hover-gray b">Team</div>
     <div className="pointer mr3 hover-gray b">Contact</div>
   </div>
