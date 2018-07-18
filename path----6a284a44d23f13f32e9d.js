e6cf57283877527e5938a6e393590f3258cf2255
Jikku Jose <jikkujose@gmail.com>
2018-07-19 05:02:46 +0530

    Updates

--- path----6a284a44d23f13f32e9d.js
+++ path----6a284a44d23f13f32e9d.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{107:function(t,a){t.exports={data:{site:{siteMetadata:{title:"OrigoChain: Land Records"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{108:function(t,a){t.exports={data:{site:{siteMetadata:{title:"OrigoChain: Land Records"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----6a284a44d23f13f32e9d.js.map
